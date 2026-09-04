84640b5b17faaae6df0baee5bd9fea055a9d3558
anup agrawal <anupagrawal895@gmail.com>
2026-09-04 19:13:22 +0530

    Time: 28 ms (52.04%), Space: 30.6 MB (67.61%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -1,32 +1,10 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        freq = {}
-        missing_num = []
+        my_set = set(nums)
+        ans = []
+        for i in range(1, len(nums) + 1):
+            if i not in my_set:
+                ans.append(i)
 
-        for num in nums:
-            freq[num] = freq.get(num, 0) + 1
-        for num in range(1, n+1):
-            if freq.get(num,0) == 0:
-                missing_num.append(num)
-
-        return missing_num
-
-# Frequency Map Approach:
-# 1. Count frequency of every number present in nums.
-# 2. Loop from 1 to n because these numbers should exist.
-# 3. If freq.get(num, 0) == 0, num is missing.
-# 4. Store missing numbers in missing_num and return it.
-#
-# .get(num, 0) returns 0 if num is not present in the dictionary.
-# Time: O(n) | Space: O(n)
-
-# TC: O(n)
-# First loop runs n times to count frequencies.
-# Second loop also runs n times to check numbers 1 to n.
-# O(n) + O(n) = O(n).
-
-# SC: O(n)
-# freq dictionary can store up to n numbers.
-# missing_num list can also store up to n numbers.
-# O(n) + O(n) = O(n) extra space.
+        return ans
+        
