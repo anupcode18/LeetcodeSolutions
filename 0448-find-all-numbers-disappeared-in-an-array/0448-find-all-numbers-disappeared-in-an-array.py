e50b2ff7a023e0ea3114d146fd5f32a51d51956d
anup agrawal <anupagrawal895@gmail.com>
2026-09-04 19:07:53 +0530

    Time: 52 ms (11.24%), Space: 30.6 MB (64.97%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -12,5 +12,21 @@ class Solution:
 
         return missing_num
 
+# Frequency Map Approach:
+# 1. Count frequency of every number present in nums.
+# 2. Loop from 1 to n because these numbers should exist.
+# 3. If freq.get(num, 0) == 0, num is missing.
+# 4. Store missing numbers in missing_num and return it.
+#
+# .get(num, 0) returns 0 if num is not present in the dictionary.
+# Time: O(n) | Space: O(n)
 
-                
+# TC: O(n)
+# First loop runs n times to count frequencies.
+# Second loop also runs n times to check numbers 1 to n.
+# O(n) + O(n) = O(n).
+
+# SC: O(n)
+# freq dictionary can store up to n numbers.
+# missing_num list can also store up to n numbers.
+# O(n) + O(n) = O(n) extra space.
