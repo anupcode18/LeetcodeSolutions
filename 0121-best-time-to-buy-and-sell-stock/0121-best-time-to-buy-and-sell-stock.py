ced6f509b738708c19024ac0e1c4e8a2fe9a839c
anup agrawal <anupagrawal895@gmail.com>
2026-09-10 16:39:58 +0530

    Time: 35 ms (79.7%), Space: 28.4 MB (99.37%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,7 +4,7 @@ class Solution:
         max_profit = 0
         min_price = prices[0]
 
-        for i in range(n):
+        for i in range(1,n):
             if prices[i] < min_price:
                 min_price = prices[i]
 
