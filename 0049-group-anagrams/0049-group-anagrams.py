8075634913e333072eaeb6d387e1a7e3c6f57eab
anup agrawal <anupagrawal895@gmail.com>
2026-09-17 23:13:12 +0530

    Time: 7 ms (98.28%), Space: 21.6 MB (97.73%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,12 @@
 class Solution:
-    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        anagram_map = defaultdict(list)
-
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        map={}
         for word in strs:
-            sorted_word = ''.join(sorted(word))
-            anagram_map[sorted_word].append(word)
-
-        return list(anagram_map.values())
+            sorted_word=''.join(sorted(word))
+            if sorted_word in map:
+                map[sorted_word].append(word)
+            else:
+                map[sorted_word]=[word]
+        return list(map.values())
+        
+        
