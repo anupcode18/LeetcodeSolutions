468a199990b25890bd614c637a4bd7f083118a7b
anup agrawal <anupagrawal895@gmail.com>
2026-09-18 00:26:08 +0530

    Time: 0 ms (100%), Space: 19.2 MB (57.55%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,9 +1,12 @@
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0    # k is var, we are declaring at oth index in nums
-        for i in range(len(nums)):
+    def removeElement(self, nums: list[int], val: int) -> int:
+        k = 0  ## tells us where to put next valid element
+        i = 0  ## scans every element
+    
+        while i < len(nums):
             if nums[i] != val:
-                nums[k] = nums[i]
-                k+=1
+                nums[k] = nums[i]  ## if cond true then change values
+                k += 1
+            i += 1
         return k
         
