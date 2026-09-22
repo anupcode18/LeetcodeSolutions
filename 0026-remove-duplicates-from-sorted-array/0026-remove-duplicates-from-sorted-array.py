26757f9072553eaf7942cb180d143457c995f736
anup agrawal <anupagrawal895@gmail.com>
2026-09-23 00:44:57 +0530

    Time: 2 ms (50.03%), Space: 20.6 MB (43.7%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,20 +1,11 @@
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
-        i = 0   ## points to the last unique element
-        j = i + 1 ## scans through the array and find next unique elem
+
+        slow = 0  # index of the last unique element placed
         
-        while j < len(nums):  
-            if nums[j] != nums[i]:  ## duplicate value get ignored
-                i+=1
-                nums[i]= nums[j]
-            j += 1  ## for next unique element
+        for fast in range(1, len(nums)):
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
 
-        return i + 1  
-""" if i = 2 (index) there will be total three elements 0,1,2 
-return i + 1 => 2 + 1 => 3  """       
-         
-
-
-
-
-        
+        return slow + 1
