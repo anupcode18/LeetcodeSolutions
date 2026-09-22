e63314783e237be7ed947fc4f9cebbfe5b8b7588
anup agrawal <anupagrawal895@gmail.com>
2026-09-22 17:25:33 +0530

    Time: 38 ms (90.06%), Space: 19.2 MB (89.24%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -3,13 +3,15 @@ class Solution:
         i, j = 0, 0
         res = []
 
+## the wile loop run until word1 and word2 has words 
+## if i or j beocme greater than len of i or j the loop stops
         while i < len(word1) and j < len(word2):
             res.append(word1[i])
             res.append(word2[j])
             i+=1
             j+=1
-        res.append(word1[i:])
-        res.append(word2[j:])
+        res.append(word1[i:]) ## for remaining words in word1 
+        res.append(word2[j:]) ## for remaining words in word2 
 
         return "".join(res)
         
