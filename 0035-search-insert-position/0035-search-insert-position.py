21fef08ff11dbed95a0a65d0a01468bba2f66664
anup agrawal <anupagrawal895@gmail.com>
2026-09-23 23:17:15 +0530

    Time: 0 ms (100%), Space: 20 MB (12.1%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,18 @@
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        n = len(nums)
+        lb = n
+        low, high = 0, n-1
+
+        while low <= high:
+            mid = (low+high)//2
+            if nums[mid] >= target:
+                lb = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        return lb
+
+
+        
+        
