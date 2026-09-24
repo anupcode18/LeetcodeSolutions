cd00427a49cc25df34b53db94d6e74d08129af03
anup agrawal <anupagrawal895@gmail.com>
2026-09-24 09:08:34 +0530

    Time: 0 ms (100%), Space: 20 MB (45.16%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -3,16 +3,12 @@ class Solution:
         n = len(nums)
         lb = n
         low, high = 0, n-1
-
-        while low <= high:
+        while low <= high:  ## why low <= high?
             mid = (low+high)//2
             if nums[mid] >= target:
-                lb = mid
+                lb = mid ## updating lb mid's index from n 
                 high = mid - 1
             else:
                 low = mid + 1
         return lb
-
-
-        
         
